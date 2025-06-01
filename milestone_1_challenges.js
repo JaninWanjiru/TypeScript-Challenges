4a5981b70ce405dd05945e23acb0decc58239de5
JaninWanjiru <janinndongo02@gmail.com>
2025-06-01 11:43:05 +0300

    Complete challenge 2:Convert minutes into seconds

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -1,5 +1,13 @@
+// Challenge 1: Return Sum of two numbers
 function addition(num1, num2) {
     return num1 + num2;
 }
 console.log(addition(3, 5));
 console.log(addition(-6, 9));
+// Challenge 2: Convert Minutes into Seconds
+function convert(minutes) {
+    var seconds = minutes * 60;
+    return seconds;
+}
+console.log(convert(5));
+console.log(convert(2));
--- milestone_1_challenges.ts
+++ milestone_1_challenges.ts
@@ -1,5 +1,15 @@
+// Challenge 1: Return Sum of two numbers
 function addition (num1: number, num2: number) {
     return num1 + num2;
 }
 console.log(addition(3, 5));
 console.log(addition(-6, 9));
+
+// Challenge 2: Convert Minutes into Seconds
+function convert (minutes: number):number {
+    const seconds = minutes * 60;
+    return seconds;
+}
+console.log(convert(5));
+console.log(convert(2));
+
