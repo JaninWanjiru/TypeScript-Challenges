d96631f7b28545ba2a33f24693217e98b5237bcb
JaninWanjiru <janinndongo02@gmail.com>
2025-06-01 14:22:14 +0300

    Complete challenge 15: Sum of Digits

--- milestone_1_challenges.ts
+++ milestone_1_challenges.ts
@@ -171,3 +171,16 @@ function sumMultiples(n: number, divisor: number): number {
 console.log(sumMultiples(10, 2));
 console.log(sumMultiples(15, 3));
 console.log(sumMultiples(7, 5));
+
+// Challenge 15: Sum of Digits
+function sumDigits(num: number): number {
+  let sum = 0;
+  while (num > 0) {
+    sum = sum + (num % 10);
+    num = Math.floor(num / 10);
+  }
+  return sum;
+}
+console.log(sumDigits(123));
+console.log(sumDigits(4567));
+console.log(sumDigits(0));
